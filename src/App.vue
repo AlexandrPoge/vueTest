a90d4b65b80200d2358d08fd767ac93108bee76c
alexanderpoge <flekssando@mail.ru>
2025-01-21 22:17:53 +0300

    fix: rename file

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import FetchItems from "@/components/FetchItems.vue";
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from "@/components/blockWithFetch.vue";
 import BusinessLunch from "@/components/BusinessLunch.vue";
 </script>
 
